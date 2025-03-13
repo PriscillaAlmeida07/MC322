7fdb390fa9d1ec06f2e0276ae7235ee274a14a21
Ana-Luiza-Gomes <a242389@dac.unicamp.br>
2025-03-13 15:50:13 -0300

    inicializacao do robo 1 na classe main

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Robo robo1;
-        
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite o nome do robô: ");
+        String nome = entrada.nextLine();
+        robo1 = new Robo(nome);
+
+        Scanner posicaoX = new Scanner(System.in);
+        System.out.print("Digite a nova posição X: ");
+        String posicao = entrada.nextLine();
+        mover()
+
     }
 }
