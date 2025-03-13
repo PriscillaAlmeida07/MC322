4aadb93302c9584718511ea0e904c726040dca89
Ana-Luiza-Gomes <a242389@dac.unicamp.br>
2025-03-13 15:41:55 -0300

    1a tentativa classe ambiente

--- Ambiente.java
+++ Ambiente.java
@@ -1,6 +1,17 @@
 public class Ambiente{
-    private int largura;
-    private int altura;
 
+    private static int largura;
+    private static int altura;
+
+    public Ambiente() {
+        largura = 100;
+        altura = 100;
+    }
     
+    public int dentroDosLimites(int x, int y) {
+        if ((largura >= x && x >= 0) && (altura >= y && y >= 0))
+            return 1;
+        else
+            return 0;
+    }
 }
