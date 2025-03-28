bb59ac6a6db30a4baf428f9822a9745457b108d5
Ana-Luiza-Gomes <a242389@dac.unicamp.br>
2025-03-28 19:55:09 -0300

    Primeira passagem dos métodos do robo flutuador

--- lab02/RoboFlutuador.java
+++ lab02/RoboFlutuador.java
@@ -4,10 +4,32 @@ public class RoboFlutuador extends RoboAereo {
     private int subidaMaxima;
     private int descidaMaxima;
 
-    // Construtor;
+    // Construtor.
     public RoboFlutuador(String nome){
         super(nome);
         subidaMaxima = 10;
         descidaMaxima = 5;
+    }
+
+    // Confere a velocidade de subida antes de realizar o movimento.
+    @Override
+    public void subir(int deltaZ){
+        if (deltaZ <= subidaMaxima){
+            super.subir(deltaZ);
+        } else { // Ele é incapaz de subir o tanto indicado
+            System.out.println("O robo tentou subir mais do que o permitido, ao invéz disso, subirá" + subidaMaxima);
+            super.subir(subidaMaxima);
+        }
+    }
+
+    // Confere a velocidade de descida antes de realizar o movimento.
+    @Override
+    public void descer(int deltaZ){
+        if (deltaZ <= descidaMaxima){
+            super.descer(deltaZ);
+        } else { // Ele é incapaz de descer o tanto indicado
+            System.out.println("O robo tentou descer mais do que o permitido, ao invéz disso, descerá" + descidaMaxima);
+            super.descer(descidaMaxima);
+        }
     }
 }
