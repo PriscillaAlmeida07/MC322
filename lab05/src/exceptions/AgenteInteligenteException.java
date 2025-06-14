0997f0b0661bb5ee500c9d3031617e8e9f96d612
Priscilla Almeida <p176205@dac.unicamp.br>
2025-06-14 20:42:15 -0300

    excluindo uma exceção que havia criado mas ela é desnecessaria

--- lab05/src/exceptions/AgenteInteligenteException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class AgenteInteligenteException extends Exception{
-    public AgenteInteligenteException(){
-        super("Tentativa de matar um Agente Inteligente"); // Agentes Inteligentes não poderão ser mortos
-    }
-}
